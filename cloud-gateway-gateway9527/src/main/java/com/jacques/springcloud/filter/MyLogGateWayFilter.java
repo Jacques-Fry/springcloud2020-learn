246b80cc28973b664c6b9e2623788ef6721603a9
Jacques-Fry <jack_yd@qq.com>
2020-10-17 22:27:56 +0800

    gateway网关全局过滤器配置

--- cloud-gateway-gateway9527/src/main/java/com/jacques/springcloud/filter/MyLogGateWayFilter.java
+++ cloud-gateway-gateway9527/src/main/java/com/jacques/springcloud/filter/MyLogGateWayFilter.java
@@ -1,10 +1,45 @@
 package com.jacques.springcloud.filter;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
 /**
  * @author Jacques·Fry
  * @version 1.0
- * @info 自定义过滤器
+ * @info 自定义全局过滤器
  * @date 2020/10/17 22:16
  */
-public class MyLogGateWayFilter {
+@Slf4j
+@Component
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("进入 MyLogGateWayFilter 过滤器" + new Date());
+
+        //token验证
+        String token = exchange.getRequest().getHeaders().getFirst("token");
+        if (StringUtils.isEmpty(token)) {
+            log.info("token不能为空");
+            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
